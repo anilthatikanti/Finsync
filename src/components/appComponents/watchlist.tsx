2ed5531f9f1e06e6bbca13d11cc2ba44f7f74790
anil thatikanti <anil.thatikanti@tandemloop.in>
2025-06-15 22:47:22 +0530

    updated delete button loader

--- src/components/appComponents/watchlist.tsx
+++ src/components/appComponents/watchlist.tsx
@@ -11,8 +11,8 @@ const WatchList: React.FC = () => {
   const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState<searchStock[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [deleteBtnLoading, setDeleteBtnLoading] = useState<boolean>(false);
-  const [updatedBtnLoading, setUpdatedBtnLoading] = useState<boolean>(false);
+  const [deleteBtnLoading, setDeleteBtnLoading] = useState<string>("");
+  const [updatedBtnLoading, setUpdatedBtnLoading] = useState<string>("");
 
   const activeWatchList = useMemo(
     () => watchList.find((wl) => wl._id === activeTab?._id),
@@ -70,7 +70,7 @@ const WatchList: React.FC = () => {
 
   const updateWatchListStock = useCallback(
     async (stockItem: searchStock) => {
-      setUpdatedBtnLoading(true);
+      setUpdatedBtnLoading(stockItem.symbol);
       try {
         const res = await axiosInstance.patch("/stocks/add-watchlist", {
           watchListId: activeTab?._id,
@@ -119,7 +119,7 @@ const WatchList: React.FC = () => {
       } catch (err) {
         console.error("Update WatchList Error:", err);
       } finally {
-        setUpdatedBtnLoading(false);
+        setUpdatedBtnLoading("");
       }
     },
     [activeTab, setQuery]
@@ -127,7 +127,7 @@ const WatchList: React.FC = () => {
 
   const deleteStockFromWatchList = useCallback(
     async (stockItem: searchStock) => {
-      setDeleteBtnLoading(true);
+      setDeleteBtnLoading(stockItem.symbol);
       try {
         const response = await axiosInstance.patch("/stocks/del-watchlist", {
           watchListId: activeTab?._id,
@@ -163,7 +163,7 @@ const WatchList: React.FC = () => {
       } catch (err) {
         console.error("Delete Stock from WatchList Error:", err);
       } finally {
-        setDeleteBtnLoading(false);
+        setDeleteBtnLoading("");
       }
     },
     [activeTab, setQuery]
@@ -281,58 +281,55 @@ const WatchList: React.FC = () => {
                       .map((s) => s.symbol)
                       .includes(stock.symbol) ? (
                       <button
-                        disabled={deleteBtnLoading}
-                        className="border-1 border-solid  border-red-500 py-[2px] px-[8px]  text-red-500 hover:bg-red-500  hover:text-white"
+                        disabled={deleteBtnLoading === stock.symbol}
+                        className="border-1 border-solid  border-red-500 h-[20px] w-[20px] text-red-500 hover:bg-red-500  hover:text-white flex justify-center items-center"
                         onClick={() => deleteStockFromWatchList(stock)}
                       >
-                        {deleteBtnLoading ? (
-                          <div role="status">
-                            <svg
-                              aria-hidden="true"
-                              className="w-3 h-3 me-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                              viewBox="0 0 100 101"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                fill="currentColor"
-                              />
-                              <path
-                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                fill="currentFill"
-                              />
-                            </svg>
-                          </div>
+                        {deleteBtnLoading === stock.symbol ? (
+                          <svg
+                            aria-hidden="true"
+                            className="w-3 h-3 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                              fill="currentColor"
+                            />
+                            <path
+                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                              fill="currentFill"
+                            />
+                          </svg>
                         ) : (
                           "-"
                         )}
                       </button>
                     ) : (
                       <button
-                        disabled={updatedBtnLoading}
-                        className="border-1 border-solid border-indigo-500 px-1.5 pb-[2px] text-indigo-500 hover:bg-indigo-500 hover:text-white"
+                        disabled={updatedBtnLoading === stock.symbol}
+                        style={{ font: "status-bar" }}
+                        className=" border-1 border-solid border-indigo-500 h-[20px] w-[20px] text-indigo-500 hover:bg-indigo-500 hover:text-white flex justify-center items-center"
                         onClick={() => updateWatchListStock(stock)}
                       >
-                        {updatedBtnLoading ? (
-                          <div role="status">
-                            <svg
-                              aria-hidden="true"
-                              className="w-3 h-3 me-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                              viewBox="0 0 100 101"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                fill="currentColor"
-                              />
-                              <path
-                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                fill="currentFill"
-                              />
-                            </svg>
-                          </div>
+                        {updatedBtnLoading === stock.symbol ? (
+                          <svg
+                            aria-hidden="true"
+                            className="w-3 h-3 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                              fill="currentColor"
+                            />
+                            <path
+                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                              fill="currentFill"
+                            />
+                          </svg>
                         ) : (
                           "+"
                         )}
